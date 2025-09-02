9c492fdf6bda4913e9bd63c8984c2f6b648e225b
OddKRop <34567100+OddKRop@users.noreply.github.com>
2025-09-02 08:28:18 +0000

    dagens økt

---
+++ myapp/src/lib/components/HeroSection.svelte
@@ -0,0 +1,10 @@
+<section class="hero">
+    <div class="hero-showcase text-center bold">
+        <h1 class="purple">MOVE TO</h1>
+        <h1 class="purple mb-xs">SPAIN</h1>
+        <h4 class="italic semi-bold">
+            There is no need to make the same mistake I made.
+        </h4>
+        
+    </div>
+</section>

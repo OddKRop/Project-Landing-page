767016d6ac6010ee79216cd136737e592707291e
OddKRop <34567100+OddKRop@users.noreply.github.com>
2025-09-04 10:38:14 +0000

    new changes

--- myapp/src/lib/components/Button.svelte
+++ myapp/src/lib/components/Button.svelte
@@ -1,8 +1,28 @@
 <script>
-    let { children } = $props();
+    let { children, ...props } = $props();
 
 
 </script>
 
 
-<button>{@render children()}</button>
+<button {...props}>{@render children()}</button>
+
+
+<style>
+    button {
+        background-color: black;
+        color: white;
+        padding: 20px 24px;
+        font-weight: normal;
+        font-size: 22px;
+        text-transform: uppercase;
+        transition: all 0.3s;
+        border: 1px solid white;
+    }
+
+    button:hover {
+        background-color: white;
+        color: black;
+    }
+
+</style>
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
    // import HeroSection from "../lib/components/HeroSection.svelte";
     import { HeroSection } from '$components'
+	import ChapterPreview from '$components/ChapterPreview.svelte';
 </script>
 
 <HeroSection />
+<ChapterPreview />
